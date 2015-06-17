77c06fd62c0d613caa3e8162ba15434125b17e21
fdai3957 <Hendrik.Pastunink@informatik.fh-fulda.de>
2015-06-17 18:31:25 +0200

    TestKart und TestButton

---
+++ Powerpuff_Kart/public_html/kartMovement.js
@@ -0,0 +1,14 @@
+var box = document.getElementById("box");
+var trans = document.getElementById("trans");
+
+box.onclick = function() {
+    
+};
+
+var keyframesArray = new Array();
+
+function fillKeyframesArray() {
+    keyframesArray.push();
+}
+fillKeyframesArray();
+
