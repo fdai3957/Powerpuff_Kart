66d0681aff4842ef55d00eb42b23e26c3c7e6be2
fdai3957 <Hendrik.Pastunink@informatik.fh-fulda.de>
2015-06-23 23:56:14 +0200

    Created Wedge with faces

--- Powerpuff_Kart/public_html/wedge.js
+++ Powerpuff_Kart/public_html/wedge.js
@@ -11,21 +11,26 @@ function createWegde(){
         new THREE.Vector3(-4, 4, -3),
         new THREE.Vector3(4, 4, -3),
         new THREE.Vector3(0, 4, 3),
-        new THREE.Vector3(-4, 4, -3),
-        new THREE.Vector3(-4, -4, -3),
-        new THREE.Vector3(0, -4, 3),
-        new THREE.Vector3(0, 4, 3),
-        new THREE.Vector3(0, -4, 3),
-        new THREE.Vector3(4, -4, -3),
         new THREE.Vector3(-4, -4, -3),
         new THREE.Vector3(4, -4, -3),
-        new THREE.Vector3(4, 4, -3)
+        new THREE.Vector3(0, -4, 3)
+    );
+    
+    geo.faces.push(
+        new THREE.Face3(2, 1, 0), 
+        new THREE.Face3(3, 5, 2),
+        new THREE.Face3(3, 2, 0),
+        new THREE.Face3(5, 4, 1),
+        new THREE.Face3(5, 1, 2),
+        new THREE.Face3(0, 1, 4),
+        new THREE.Face3(0, 4, 3),
+        new THREE.Face3(5, 3, 4)
     );
     
     var mat = new THREE.LineBasicMaterial({
         color: 0x33CC33
     });
     
-    return new THREE.Line(geo, mat);
+    return new THREE.Mesh(geo, mat);
 }
 
