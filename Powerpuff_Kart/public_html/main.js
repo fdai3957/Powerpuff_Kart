f973aca44ecf0a8a440c3c28acf9cd7b2c934d36
JanaSowieso <janabutorac@googlemail.com>
2015-09-28 17:18:50 +0200

    Plane auskommentiert

--- Powerpuff_Kart/public_html/main.js
+++ Powerpuff_Kart/public_html/main.js
@@ -90,14 +90,14 @@ $(function () {
     
 
     //Ground
-    var planeGeometry = new THREE.PlaneGeometry(30, 30, 30);
-    var planeMaterial = new THREE.MeshLambertMaterial({color: 0xffffff});
-    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-
-    plane.rotation.x = -0.5 * Math.PI;
-    plane.receiveShadow = true;
-
-    scene.add(plane);
+//    var planeGeometry = new THREE.PlaneGeometry(30, 30, 30);
+//    var planeMaterial = new THREE.MeshLambertMaterial({color: 0xffffff});
+//    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+//
+//    plane.rotation.x = -0.5 * Math.PI;
+//    plane.receiveShadow = true;
+//
+//    scene.add(plane);
 
 
     //Light
